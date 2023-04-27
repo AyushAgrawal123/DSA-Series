2d237b918d45d5c1c0888f66c300f396d452d360
AyushAgrawal123 <106971578+AyushAgrawal123@users.noreply.github.com>
2023-04-27 20:30:57 +0530

    Create Binary_Search_Tree.cpp
    Binary_Search Tree Implementation

---
+++ Binary_Search_Tree.cpp
@@ -0,0 +1,222 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+Node *minValue(Node *root)
+{
+    Node *temp = root;
+
+    while (temp->left != NULL)
+    {
+        temp = temp->left;
+    }
+    return temp;
+}
+Node *maxValue(Node *root)
+{
+    Node *temp = root;
+
+    while (temp->right != NULL)
+    {
+        temp = temp->right;
+    }
+    return temp;
+}
+Node *insertIntoBST(Node *root, int data)
+{
+    if (root == NULL)
+    {
+        root = new Node(data);
+        return root;
+    }
+
+    if (data > root->data)
+    {
+        root->right = insertIntoBST(root->right, data);
+    }
+
+    if (data < root->data)
+    {
+        root->left = insertIntoBST(root->left, data);
+    }
+    return root;
+}
+Node *deleteFromBST(Node *root, int val)
+{
+    if (root == NULL)
+    {
+        return root;
+    }
+    if (root->data == val)
+    {
+        // 0 child
+        if (root->left == NULL && root->right == NULL)
+        {
+            delete root;
+            return NULL;
+        }
+
+        // 1 child
+        // left child
+        if (root->left != NULL && root->right == NULL)
+        {
+            Node *temp = root->left;
+            delete root;
+            return temp;
+        }
+        // right child
+        if (root->left == NULL && root->right != NULL)
+        {
+            Node *temp = root->right;
+            delete root;
+            return temp;
+        }
+        // 2 child
+        if (root->left != NULL && root->right != NULL)
+        {
+            // find min value from right subtree or max value from left subtree
+            int mini = minValue(root->right)->data;
+            root->data = mini;
+            root->right = deleteFromBST(root->right, mini);
+            return root;
+        }
+    }
+    else if (root->data > val)
+    {
+        root->left = deleteFromBST(root->left, val);
+        return root;
+    }
+    else
+    {
+        root->right = deleteFromBST(root->right, val);
+        return root;
+    }
+}
+
+
+
+void takeInput(Node *&root)
+{
+    int data;
+    cin >> data;
+
+    while (data != -1)
+    {
+        root = insertIntoBST(root, data);
+        cin >> data;
+    }
+}
+
+void levelOrderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+
+    while (!q.empty())
+    {
+        Node *temp = q.front();
+        q.pop();
+        if (temp == NULL)
+        {
+            // previous level complete
+            cout << endl;
+            if (!q.empty()) // node still have some child nodes
+            {
+                q.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout << temp->data << " ";
+            if (temp->left)
+            {
+                q.push(temp->left);
+            }
+
+            if (temp->right)
+            {
+                q.push(temp->right);
+            }
+        }
+    }
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    // left root right
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+void preorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    //  root left right
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    // left right root
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = NULL;
+    cout << "Enter data for creating BST " << endl;
+    takeInput(root);
+     
+
+     // Deletion
+     root = deleteFromBST(root, 30);
+
+    cout << "Level Order traversal is " << endl;
+    levelOrderTraversal(root);
+
+    cout << "Inorder traversal is " << endl;
+    inorder(root);
+    cout << endl;
+    cout << "preorder traversal is " << endl;
+    preorder(root);
+    cout << endl;
+    cout << "postorder traversal is " << endl;
+    postorder(root);
+    cout << endl;
+
+    cout << "Min value is " << minValue(root)->data << endl;
+    cout << "Max value is " << maxValue(root)->data << endl;
+
+      
+    return 0;
+}
